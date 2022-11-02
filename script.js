b53b07aa275b8c94fc82c97d4a84ba3a598199f2
Kristina <alena@MacBook-Air-Kristina.local>
2022-11-03 00:58:22 +0300

    add popup

--- script.js
+++ script.js
@@ -25,7 +25,41 @@ const initialCards = [
     }
     ];
 
+const profilePopup = document.querySelector('#profilePopup');
+const formElement = document.querySelector('#profileForm');
+const popupButtonOpen = document.querySelector('.profile__edit-button');
+const profileFullname = document.querySelector('#profileUsername');
+const profileJob = document.querySelector('#profileDescription');
+const nameInput = document.querySelector('#formsName');
+const jobInput = document.querySelector('#formsDescription');
+
+const cardTemplate = document.querySelector('#cardTemplate');
+const cardsList = document.querySelector('.elements');
+const cardBlock = document.querySelector('.cards').cloneNode(true);
+
+
+const addButtonOpen = document.querySelector('.profile__add-button'):
+const cardAddPopup = document.querySelector('#cardAddPopup');
+const addFormElement = document.querySelector('#formAdd');
+const placeInput = document.querySelector('.forms__mesto'); <!-- возможно # -->
+const linkInput = document.querySelector('.popup__caption');
+
+const imageModal = document.querySelector('#imagePopup');
+const imagePop = document.querySelector('.popup__image');
+const captionPop = document.querySelector('.popup__caption');
+
+const closeButtons = document.querySelectorAll('.popup__close-button');
+
 const profileName = {
-    name: '';
-    
+  name: 'Жак-Ив Кусто',
+  occupation: 'Исследователь океана'
+}
+
+
+//удаление
+function deleteCard (defaultCard) {
+  defaultCard.querySelector('.cards__delete').addEventListener('click', () => {
+    const listItem = defaultCard.closest('.cards');
+    listItem.delete();
+  });
 }
